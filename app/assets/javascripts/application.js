a6522d60f24530f98a5ac5216d0004646c9df22a
rudzainy <rudzainy@gmail.com>
2017-07-23 02:01:26 +0800

    Disable turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,7 +20,7 @@
 //= require react
 //= require react_ujs
 //= require components
-//= require turbolinks
+// require turbolinks
 //= require tinymce-jquery
 //= require bootstrap-typeahead-rails
 //= require_tree .
