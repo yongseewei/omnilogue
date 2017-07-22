786732a1a54bfa57cdb9ec69b68fb894ad498690
yongseewei <yongseewei@gmail.com>
2017-07-22 13:52:13 +0800

    completed facebook login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -223,7 +223,7 @@ Devise.setup do |config|
   # Turn scoped views on. Before rendering "sessions/new", it will first check for
   # "users/sessions/new". It's turned off by default because it's slower if you
   # are using only default views.
-  # config.scoped_views = false
+  config.scoped_views = true
 
   # Configure the default scope given to Warden. By default it's the first
   # devise role declared in your routes (usually :user).
@@ -274,5 +274,5 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
-  config.omniauth :facebook, ENV['APP_KEY'], ENV['APP_SECRET']
+  config.omniauth :facebook, ENV['APP_KEY'], ENV['APP_SECRET'], scope: 'email', info_fields: 'email, first_name, last_name'
 end
