7528b8219e056b56659112b0719de067ae2cb0e1
yongseewei <yongseewei@gmail.com>
2017-07-23 19:50:07 +0800

    added authentication to questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,5 @@
 class QuestionsController < ApplicationController
+  prepend_before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   def index
     @questions = Question.all.map{ |question| question.to_json }
     @categories = Category.all.map{ |category| category.to_json}
