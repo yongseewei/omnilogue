689c657ee68d943e2d555260dc470a38cfc79873
hewrin <hewrin@hotmail.com>
2017-07-23 11:33:33 +0800

    Add comment voting

--- app/assets/javascripts/components/comments/comment_card.es6.jsx
+++ app/assets/javascripts/components/comments/comment_card.es6.jsx
@@ -8,6 +8,7 @@ class CommentCard extends React.Component {
           <img src="http://propeller.in/assets/images/avatar-icon-40x40.png" width="20" height="20" /> { `Commented by ${comment.user.username} ${moment(comment.created_at).fromNow()}` }
         </div>
         { comment.content }
+        { <VoteBox votable = { comment } modelName="Comment" /> }
       </div>
     )
   }
